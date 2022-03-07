bdcc75db89ce4dda9f500dd415a1180df72bb40d
Marcela-Menezes2 <mmenezsi.825369@gmail.com>
2022-03-07 09:54:12 -0300

    criando layout principal

--- Moeda Digital/Moeda Digital/ViewController.swift
+++ Moeda Digital/Moeda Digital/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    var moedaScreen:MoedaScreen?
+ 
+    override func loadView() {
+        self.moedaScreen = MoedaScreen()
+        self.view = self.moedaScreen
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .black
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+     self.navigationController?.setNavigationBarHidden(true, animated: true)
 
+   
 }
-
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    
+  
+    }
+    
+}
